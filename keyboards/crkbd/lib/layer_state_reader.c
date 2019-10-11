b9ce5bb7b1e898a308f122da59a705bc32b34ae0
Roman Dittrich <rdittrich@salmonsoftware.ie>
2019-10-11 10:15:37 +0200

    Updated layer state reader.

--- keyboards/crkbd/lib/layer_state_reader.c
+++ keyboards/crkbd/lib/layer_state_reader.c
@@ -4,19 +4,21 @@
 #include "crkbd.h"
 
 // in the future, should use (1U<<_LAYER_NAME) instead, but needs to be moved to keymap,c
-#define L_BASE 0
-#define L_LOWER 2
-#define L_RAISE 4
-#define L_ADJUST 8
-#define L_ADJUST_TRI 14
+#define L_WORKMAN (1 << _WORKMAN)
+#define L_LOWER (1 << _LOWER)
+#define L_RAISE (1 << _RAISE)
+#define L_ADJUST (1 << _ADJUST)
+#define L_ADJUST_TRI (L_ADJUST|L_LOWER|L_RAISE)
+#define L_ESCFN (1 << _ESCFN)
+#define L_QWERTY (1 << _QWERTY)
 
 char layer_state_str[24];
 
 const char *read_layer_state(void) {
   switch (layer_state)
   {
-  case L_BASE:
-    snprintf(layer_state_str, sizeof(layer_state_str), "Layer: Default");
+  case L_WORKMAN:
+    snprintf(layer_state_str, sizeof(layer_state_str), "Layer: _WORKMAN");
     break;
   case L_RAISE:
     snprintf(layer_state_str, sizeof(layer_state_str), "Layer: Raise");
@@ -27,6 +29,12 @@ const char *read_layer_state(void) {
   case L_ADJUST:
   case L_ADJUST_TRI:
     snprintf(layer_state_str, sizeof(layer_state_str), "Layer: Adjust");
+    break;
+  case L_ESCFN:
+    snprintf(layer_state_str, sizeof(layer_state_str), "Layer: Fn");
+    break;
+  case L_QWERTY:
+    snprintf(layer_state_str, sizeof(layer_state_str), "Layer: QWERTY");
     break;
   default:
     snprintf(layer_state_str, sizeof(layer_state_str), "Layer: Undef-%ld", layer_state);
